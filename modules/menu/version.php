188cad9f3bca65b89c6d2429e2378599ae0ed316
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2017-10-20 20:09:12 +0700

    beta 1

---
+++ modules/menu/version.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2014 VINADES ., JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Dec 3, 2010 11:24:58 AM
+ */
+
+$module_version = array(
+    'name' => 'Module Menu',
+    'modfuncs' => '',
+    'submenu' => '',
+    'is_sysmod' => 1,
+    'virtual' => 0,
+    'version' => '4.2.01',
+    'date' => 'Sat, 5 Aug 2017 13:00:00 GMT',
+    'author' => 'VINADES <contact@vinades.vn>',
+    'note' => '',
+    'uploads_dir' => array(
+        $module_upload
+    )
+);
